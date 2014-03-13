4f1fdf114b6c1a6f82f445b83bd02ddf1ea9534b
Christopher De Cairos <cade@mozillafoundation.org>
2014-03-13 11:32:57 -0400

    hacking asset loading :/

--- assets/javascripts/webmaker.js
+++ assets/javascripts/webmaker.js
@@ -24,5 +24,7 @@ Discourse.LoginView.reopen({
 // TODO:
 // configurable script locations && better script loading!!!!
 $.getScript("https://login.persona.org/include.js", function() {
-  $.getScript("http://localhost:7777/bower/webmaker-auth-client/dist/webmaker-auth-client.min.js");
+  $.getScript("http://localhost:7777/bower/eventEmitter/EventEmitter.js", function() {
+    $.getScript("http://localhost:7777/bower/webmaker-auth-client/webmaker-auth-client.js");
+  });
 });
